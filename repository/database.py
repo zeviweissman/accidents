a0f77544fb06930e9d564a08f818af8e4c110e58
nobody <yzeviw@gmail.com>
2024-10-10 09:46:05 +0300

    finshed setting up database connection and tested it to make sure the connection works

---
+++ repository/database.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://localhost:27017')
+
+
+def get_db_connection():
+    return client['accidents-stats']
+
+def get_day_collection():
+    return get_db_connection()['days']
+
+def get_week_collection():
+    return get_db_connection()['weeks']
+
+def get_month_collection():
+    return get_db_connection()['month']
+
+def get_accidents_collection():
+    return get_db_connection()['accidents']
